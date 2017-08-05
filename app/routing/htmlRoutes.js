e9094a6c3f2b688c57e3ed39a8e9d6b2b355a553
jliip51 <jliip51@gmail.com>
2017-08-05 12:10:36 -0400

    fix routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,18 +1,1 @@
-var express = require('express');
-var app = express();
 
-var routes = {
-  home: app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
-  }),
-
-  reserve: app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/reserve.html"));
-  }),
-
-  tables: app.get("/table", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/table.html"));
-  })
-};
-
-module.exports = routes;
