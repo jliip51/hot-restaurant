c30c9a7ed09795e4e4c9a92cb1d5357ffbbe7099
jliip51 <jliip51@gmail.com>
2017-08-05 11:15:15 -0400

    server setup

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PATH || 3000;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
